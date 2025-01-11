85e23859a08d330955d4cedb94037635c7f6a853
Jony9720 <56095220+Jony9720@users.noreply.github.com>
2025-01-11 15:21:45 -0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,10 +1,14 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'mysql',
-    port: process.env.DB_PORT,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
 });
 
 const testConnection = async () => {
